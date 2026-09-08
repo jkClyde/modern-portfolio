911ecc8fdcdd2257323fd08614af3412857f5ad8
dev <dev@example.com>
2026-09-08 17:38:21 +0800

    updated

--- zoom-old.jsx
+++ zoom-old.jsx
@@ -33,7 +33,6 @@ const ZoomParallax = () => {
 
             imageRefs.current.forEach((el, index) => {
                 if (!el) return;
-
                 tl.fromTo(
                     el,
                     { scale: 1 },
@@ -47,19 +46,14 @@ const ZoomParallax = () => {
     }, []);
 
     return (
-        <div
-            ref={container}
-            className="relative h-[100vh] overflow-hidden"
-            id="gallery"
-        >
-            {/* CENTER CONTENT */}
+        <div ref={container} className="relative h-[100vh] overflow-hidden" id="gallery">
             <div
                 ref={(el) => (imageRefs.current[0] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
                 <div
-                    className="contentContainer md:w-[25vw] h-[23vh] md:h-[25vh] relative overflow-auto shadow flex flex-col justify-center"
+                    className="contentContainer md:w-[25vw] h-[23vh] md:h-[25vh] relative overflow-auto  shadow flex flex-col justify-center"
                     style={{
                         backgroundImage: `url(img/image.png)`,
                         backgroundSize: "cover",
@@ -67,7 +61,6 @@ const ZoomParallax = () => {
                     }}
                 >
                     <div className="absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t from-background to-transparent w-full pointer-events-none z-10"></div>
-
                     <div className="p-4 bg-black bg-opacity-70 h-full w-full flex justify-center items-center flex-col">
                         <div className="flex flex-col items-center mt-6 mb-8">
                             <p className="text-white text-[12px] sm:text-sm mb-2 opacity-80">
@@ -100,26 +93,12 @@ const ZoomParallax = () => {
                 </div>
             </div>
 
-            {/* IMAGE 3 */}
             <div
                 ref={(el) => (imageRefs.current[1] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
-                <div
-                    className="
-                        imageContainer
-                        relative
-                        w-[50vw]
-                        h-[10vh]
-                        md:w-[30vw]
-                        md:h-[30vh]
-                        -top-[18vh]
-                        md:-top-[29.5vh]
-                        left-[3.5vw]
-                        overflow-hidden
-                    "
-                >
+                <div className="imageContainer relative w-[30vw] h-[25vh] md:h-[30vh] -top-[28vh] md:-top-[29.5vh] left-[3.5vw] overflow-hidden">
                     <img
                         src="/img/w3.png"
                         alt=""
@@ -130,57 +109,25 @@ const ZoomParallax = () => {
                 </div>
             </div>
 
-            {/* IMAGE 2 */}
             <div
                 ref={(el) => (imageRefs.current[2] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
                 <div
-                    className="
-                        imageContainer
-                        relative
-                        w-[50vw]
-                        h-[10vh]
-                        md:w-[28vw]
-                        md:h-[30vh]
-                        -top-[5vh]
-                        md:-top-[10vh]
-                        -left-[40vw]
-                        md:-left-[27.5vw]
-                        overflow-hidden
-                        bg-cover
-                        bg-center
-                        bg-no-repeat
-                    "
-                    style={{
-                        backgroundImage: "url('/img/w2.png')",
-                    }}
+                    className="imageContainer relative w-[30vw] h-[30vh] md:w-[28vw] md:h-[30vh] -top-[8vh] md:-top-[10vh] -left-[32vw] md:-left-[27.5vw] overflow-hidden bg-cover bg-center bg-no-repeat"
+                    style={{ backgroundImage: "url('/img/w2.png')" }}
                 >
                     <div className="absolute inset-0 z-10 bg-black/30"></div>
                 </div>
             </div>
 
-            {/* IMAGE 1 */}
             <div
                 ref={(el) => (imageRefs.current[3] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
-                <div
-                    className="
-                        imageContainer
-                        w-[50vw]
-                        md:w-[25vw]
-                        h-[12vh]
-                        md:h-[25vh]
-                        relative
-                        left-[40vw]
-                        md:left-[26vw]
-                        -top-[5vh]
-                        md:top-0
-                    "
-                >
+                <div className="imageContainer w-[29vw] md:w-[25vw] h-[20vh] md:h-[25vh] relative left-[30vw] md:left-[26vw]">
                     <img
                         src="img/w1.png"
                         alt="image"
@@ -191,91 +138,38 @@ const ZoomParallax = () => {
                 </div>
             </div>
 
-            {/* IMAGE 4 */}
             <div
                 ref={(el) => (imageRefs.current[4] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
                 <div
-                    className="
-                        imageContainer
-                        w-[50vw]
-                        md:w-[24vw]
-                        h-[10vh]
-                        md:h-[25vh]
-                        relative
-                        top-[20vh]
-                        md:top-[27.5vh]
-                        left-[10.5vw]
-                        md:left-[2vw]
-                        bg-cover
-                        bg-center
-                        bg-no-repeat
-                        bg-top
-                    "
-                    style={{
-                        backgroundImage: "url('/img/w4.png')",
-                    }}
+                    className="imageContainer w-[25vw] md:w-[27vw] h-[25vh] relative top-[26vh] md:top-[27.5vh] left-[1.5vw] bg-cover bg-center bg-no-repeat bg-top"
+                    style={{ backgroundImage: "url('/img/w4.png')" }}
                 ></div>
             </div>
 
-            {/* IMAGE 5 */}
             <div
                 ref={(el) => (imageRefs.current[5] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
                 <div
-                    className="
-                        imageContainer
-                        w-[50vw]
-                        md:w-[24vw]
-                        h-[12vh]
-                        md:h-[25vh]
-                        relative
-                        top-[7.5vh]
-                        md:top-[21vh]
-                        -left-[45.5vw]
-                        md:-left-[25.5vw]
-                        bg-cover
-                        bg-center
-                        bg-no-repeat
-                        bg-top
-                    "
-                    style={{
-                        backgroundImage: "url('/img/w5.png')",
-                    }}
+                    className="imageContainer w-[24vw] h-[25vh] relative top-[24vh] md:top-[21vh] -left-[27.5vw] md:-left-[25.5vw] bg-cover bg-center bg-no-repeat bg-top"
+                    style={{ backgroundImage: "url('/img/w5.png')" }}
                 >
                     <div className="absolute inset-0 bg-black/30 z-10"></div>
                 </div>
             </div>
 
-            {/* IMAGE 6 */}
             <div
                 ref={(el) => (imageRefs.current[6] = el)}
                 className="flex justify-center items-center"
                 style={overlayStyle}
             >
                 <div
-                    className="
-                        imageContainer
-                        w-[50vw]
-                        md:w-[20vw]
-                        h-[10vh]
-                        md:h-[20vh]
-                        relative
-                        top-[8vh]
-                        md:top-[24vh]
-                        left-[45vw]
-                        md:left-[26vw]
-                        bg-cover
-                        bg-center
-                        bg-no-repeat
-                    "
-                    style={{
-                        backgroundImage: "url('/img/w6.png')",
-                    }}
+                    className="imageContainer w-[22vw] md:w-[20vw] h-[20vh] relative top-[20vh] md:top-[24vh] left-[33vw] md:left-[26vw] bg-cover bg-center bg-no-repeat"
+                    style={{ backgroundImage: "url('/img/w6.png')" }}
                 >
                     <div className="absolute inset-0 bg-black/50 z-10"></div>
                 </div>
